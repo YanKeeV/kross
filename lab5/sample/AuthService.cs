842915aa342d5c8aa7686ebcf4fd935e332f5484
YanKeeV <45319167+YanKeeV@users.noreply.github.com>
2023-12-06 23:30:20 +0200

    Add files via upload

---
+++ lab5/sample/AuthService.cs
@@ -0,0 +1,65 @@
+﻿using SampleMvcApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Caching.Memory;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace SampleMvcApp;
+
+public sealed class AuthService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly IMemoryCache _cache;
+
+    public AuthService(IHttpContextAccessor contextAccessor, IMemoryCache cache) => (_contextAccessor, _cache) = (contextAccessor, cache);
+
+    public bool Register(UserModel model)
+    {
+        var result = _cache.Set<UserModel>(GetCacheKey(model.emailAddress), model);
+
+        if (result is not null)
+            return true;
+
+        return false;
+    }
+
+    public async Task Logout()
+    {
+        await _contextAccessor.HttpContext.SignOutAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+
+    public bool Login(UserModel model)
+    {
+        var result = _cache.Get<UserModel>(GetCacheKey(model.emailAddress));
+
+        if (result is not null)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, result.name),
+                new Claim(ClaimTypes.Email, result.emailAddress),
+
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties();
+
+            _contextAccessor.HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties).Wait();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private string GetCacheKey(string emailAddress)
+        => $"user_{emailAddress}";
+}
---
+++ lab5/sample/Program.cs
@@ -0,0 +1,72 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.Extensions.Hosting;
+using SampleMvcApp;
+using System.Net;
+
+using Microsoft.IdentityModel.Logging;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddMemoryCache();
+
+ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+Configure(app, builder.Configuration);
+
+app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllersWithViews();
+
+    services.AddAuth0WebAppAuthentication(options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Domain"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+    });
+
+    services.Configure<CookiePolicyOptions>(options =>
+    {
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+        options.HttpOnly = HttpOnlyPolicy.None;
+        options.Secure = CookieSecurePolicy.Always;
+    });
+
+    services.AddSingleton<AuthService>();
+}
+
+void Configure(WebApplication app, IConfiguration configuration)
+{
+    app.UseStaticFiles();
+    app.UseCookiePolicy();
+
+    app.UseAuthentication();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+    }
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapDefaultControllerRoute();
+    });
+}
